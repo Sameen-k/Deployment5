d9d65699f21c8d4885ab84effa4e4354cbac1816
Sameen Khan <128739962+Sameen-k@users.noreply.github.com>
2023-10-13 22:02:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 python3.7 -m venv test
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/Sameen-k/Deployment5.git
+cd Deployment5
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
